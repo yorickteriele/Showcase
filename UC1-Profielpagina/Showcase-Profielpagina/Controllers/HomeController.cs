e4853394de92b0e08773bd9796639dde4bd14ddc
yyyorick <yorick.teriele@outlook.com>
2025-02-06 10:39:44 +0100

    ADD: Language dropdown

--- UC1-Profielpagina/Showcase-Profielpagina/Controllers/HomeController.cs
+++ UC1-Profielpagina/Showcase-Profielpagina/Controllers/HomeController.cs
@@ -15,7 +15,15 @@ namespace Showcase_Profielpagina.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var workExperiences = new List<WorkExperience>{
+                new WorkExperience("Medewerker", "2023 - 2024", "Student aan huis, Zwolle"),
+                new WorkExperience("Schaduwstage Operational Specialist Intelligence", "jan 2020 - feb 2021", "Politie Oost-Nederland"),
+                new WorkExperience("Webdesigner", "2021 - Heden", "OmmerOase, Ommen"),
+                new WorkExperience("Horecamedewerker/schoonmaker", "2021 - 2022", "Monkey Town, Zwolle"),
+                new WorkExperience("Krantenbezorger", "2020 - 2021", "All-Inn, Zwolle")
+            };
+
+            return View(workExperiences);
         }
 
         public IActionResult Privacy()
@@ -30,3 +38,15 @@ namespace Showcase_Profielpagina.Controllers
         }
     }
 }
+public class WorkExperience {
+    public string Title { get; set; }
+    public string Period { get; set; }
+    public string Institute { get; set; }
+
+    public WorkExperience(string title, string period, string institute) {
+        Title = title;
+        Period = period;
+        Institute = institute;
+    }
+}
+
