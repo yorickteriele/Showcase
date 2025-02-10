f6bdbfebaaa7ae2f797298c62eb31a209fbdf815
Yorick te Riele <yorick.teriele@outlook.com>
2025-02-10 14:19:18 +0100

    Data centralised

--- UC1-Profielpagina/Showcase-Profielpagina/Controllers/HomeController.cs
+++ UC1-Profielpagina/Showcase-Profielpagina/Controllers/HomeController.cs
@@ -14,22 +14,47 @@ namespace Showcase_Profielpagina.Controllers
         }
 
         public IActionResult Index() {
-            var workExperiences = new List<WorkExperience>{
+            var skills = new List<WorkExperience> {
+                new WorkExperience("C#", "Expert", "Programming Language"),
+                new WorkExperience("SQL", "Intermediate", "Database Management"),
+                new WorkExperience("Unity", "Advanced", "Game Development"),
+                new WorkExperience("Java", "Intermediate", "Programming Language")
+            };
+            string skillsTitle = "Vaardigheden";
+
+            ViewData["SkillsInput"] = skills;
+            ViewData["SkillsTitle"] = skillsTitle;
+
+            var projects = new List<WorkExperience> {
+                new WorkExperience("Project A", "2023", "Some description here"),
+                new WorkExperience("Project B", "2024", "Some description here")
+            };
+            string projectsTitle = "Projecten";
+
+            ViewData["ProjectsInput"] = projects;
+            ViewData["ProjectsTitle"] = projectsTitle;
+
+            var workExperiences = new List<WorkExperience> {
                 new WorkExperience("Medewerker", "2023 - 2024", "Student aan huis, Zwolle"),
                 new WorkExperience("Schaduwstage Operational Specialist Intelligence", "jan 2020 - feb 2021", "Politie Oost-Nederland"),
                 new WorkExperience("Webdesigner", "2021 - Heden", "OmmerOase, Ommen"),
                 new WorkExperience("Horecamedewerker/schoonmaker", "2021 - 2022", "Monkey Town, Zwolle"),
                 new WorkExperience("Krantenbezorger", "2020 - 2021", "All-Inn, Zwolle")
             };
+            string workExperiencesTitle = "Werkervaringen";
 
-            ViewData["Experiences"] = workExperiences;
+            ViewData["ExperiencesInput"] = workExperiences;
+            ViewData["ExperiencesTitle"] = workExperiencesTitle;
 
-            string aboutMe = "Ik ben een enthousiaste en leergierige student Software Engineering met een passie voor technologie en ontwikkeling. Nieuwe kennis opdoen en deze praktisch toepassen motiveert mij. Ik zoek actief naar manieren om mezelf te verbeteren en uitdagingen aan te gaan. Mijn doel is om met mijn vaardigheden en doorzettingsvermogen een waardevolle bijdrage te leveren aan uw organisatie.";
+            string aboutMeInput = "Ik ben een enthousiaste en leergierige student Software Engineering met een passie voor technologie en ontwikkeling. Nieuwe kennis opdoen en deze praktisch toepassen motiveert mij. Ik zoek actief naar manieren om mezelf te verbeteren en uitdagingen aan te gaan. Mijn doel is om met mijn vaardigheden en doorzettingsvermogen een waardevolle bijdrage te leveren aan uw organisatie.";
+            string aboutMe = "Over Mij";
 
-            ViewData["AboutMe"] = aboutMe;
+            ViewData["AboutMeInput"] = aboutMeInput;
+            ViewData["AboutMeTitle"] = aboutMe;
 
             return View();
         }
+
 
 
         public IActionResult Privacy()
